bdecc93825a2e6fffb4756ace3bbc268abe235c6
Ton Schoots <tschoots@blackducksoftware.com>
2015-12-13 20:44:07 +0100

    everthing pulled

--- officialImages.go
+++ officialImages.go
@@ -5,9 +5,15 @@
 	It should be run in the context of a docker-machine.
 
 	Status :
-	
+
 	   9 - 12 - 2015
 	        Alle official images tags are parsed.
+	        
+	   13 - 12 - 2015
+	   		Everthing is pulled from git.
+	   		But it should be in parellel to speed up things
+	   		And parsing of dockerfiles stil has to tacke place
+	   		And generation of .dot file
 */
 
 package main
@@ -23,13 +29,15 @@ import (
 )
 
 type input struct {
-	gitpath string
+	gitpath        string
+	dockerfilepath string
 }
 
 var in input
 
 func init() {
 	flag.StringVar(&in.gitpath, "p", "REQUIRED", "the path where a git archive can be cloned.")
+	flag.StringVar(&in.dockerfilepath, "d", "REQUIRED", "the path where the archives containing Dockerfiles should reside.")
 }
 
 const APP_VERSION = "0.1"
@@ -38,10 +46,11 @@ const APP_VERSION = "0.1"
 var versionFlag *bool = flag.Bool("v", false, "Print the version number.")
 
 type img struct {
-	Name   string
-	Latest bool
-	From   string
-	Tags   []string
+	Name           string
+	Tag            string
+	From           string
+	Childs         []string
+	DockerfilePath string
 }
 
 func IsDirEmpty(name string) (bool, error) {
@@ -81,6 +90,8 @@ func main() {
 		fmt.Println("git path : ", in.gitpath)
 	}
 
+	os.MkdirAll(in.gitpath, 0777)
+	os.MkdirAll(in.dockerfilepath, 0777)
 	err := os.Chdir(in.gitpath)
 	if err != nil {
 		fmt.Printf("error chdir : %s\n", err)
@@ -123,7 +134,15 @@ func main() {
 		fmt.Printf("Error in readdir : %s\n.", err)
 	}
 
-	r, _ := regexp.Compile("(\\S+):\\s+(git://github.com/.+)@\\S+\\s?(\\S*)")
+	// create a map with to store all images
+	images := make(map[string]img)
+	images["scratch:latest"] = img{"scratch", "latest", "", make([]string, 30), ""}
+
+	gitArchives := make(map[string]string)
+
+	// get all the image names tags and paths
+	//r, _ := regexp.Compile("(\\S+):\\s+(git://github.com/.+)@\\S+\\s?(\\S*)")
+	r, _ := regexp.Compile("(\\S+):\\s+(git://github.com/.+)@\\S+\\s?(\\S*)\\n")
 	for _, f := range files {
 		fmt.Println(f.Name())
 		content, err := ioutil.ReadFile(f.Name())
@@ -137,14 +156,55 @@ func main() {
 			//fmt.Println(len(my_matches[ar]))
 			fmt.Printf("\t%s\n", my_matches[ar][1]) // tag
 			//fmt.Printf("\t%s\n",my_matches[ar][2]) // github archive
-			//fmt.Printf("\t%s\n",my_matches[ar][3]) // dir in the archive containing docker file
-			
+			fmt.Printf("\t\t%s\n", my_matches[ar][3]) // dir in the archive containing docker file
+			// make directory and clone archive.
+			tag := my_matches[ar][1]
+			gitArch := my_matches[ar][2]
+			archPath := my_matches[ar][3]
+			archivePath := fmt.Sprintf("%s/%s", in.dockerfilepath, f.Name())
+
+			fullName := fmt.Sprintf("%s:%s", f.Name(), tag)
+			DockerfilePath := fmt.Sprintf("%s/%s/Dockerfile", archivePath, archPath)
+
+			gitArchives[gitArch] = archivePath
+			images[fullName] = img{f.Name(), tag, "", make([]string, 30), DockerfilePath}
+
 		}
 		fmt.Printf("\n\n")
 
 	}
 
+	fmt.Printf("%v\n\n", images)
+	//fmt.Printf("%v\n", gitArchives)
+
+	// now start pulling the Dockerfile archives
+
+	// parse the Dockerfiles to see where it's comming from
+	for gitUrl, localPath := range gitArchives {
+		//fmt.Printf("k: %s , v: %s\n", k, v)
+		os.MkdirAll(localPath, 0777)
+		filePath := fmt.Sprintf("%s/.git", localPath)
+		var cmd *exec.Cmd
+		if _, err := os.Stat(filePath); err == nil {
+			os.Chdir(localPath)
+			cmd = exec.Command("git",
+			"pull",
+		)
+		}else {
+			cmd = exec.Command("git",
+			"clone",
+			gitUrl,
+			localPath,
+		)
+		}
+		
+		if err := cmd.Run(); err != nil {
+			fmt.Printf("git clone went wrong : %s\n", err)
+		}
+	}
+
 	os.Chdir(in.gitpath)
-	DelWinDir(in.gitpath)
+	os.RemoveAll(in.gitpath)
+	//DelWinDir(in.gitpath)
 
 }
